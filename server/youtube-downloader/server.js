5396a7a0c3b8ff1229e78f911d1f2f610ae72bbb
alsta017 <alsta017@gmail.com>
2024-09-06 20:43:10 +0200

    fappos.server

---
+++ server/youtube-downloader/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const youtubedl = require('youtube-dl-exec');
+const logger = require('progress-estimator')()
+const port = 3000;
+
+app.use(express.static('downloads'));
+
+app.get('/download', async (req, res) => {
+  const url = req.query.url;
+  const clientId = req.query.clientId;
+
+  if (!url) {
+    return res.status(400).send('URL is required');
+  }
+
+  downloadProgress[clientId] = 0;
+
+  try{
+    const filePath = `downloads/${url.split('/').pop()}_${Date.now()}.mp4`;
+    const promise = youtubedl(url, { dumpSingleJson: true })
+    const result = await logger(promise, `Obtaining url: ${url}`)
+  }
+  catch(err){
+    console.log(err)
+  }
+
+})
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+})
